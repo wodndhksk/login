f25f7e2a29c2936636d7725d0d22fa3b9f11cd81
Jaewoo Kim <wodndhksk@gmail.com>
2022-12-19 21:20:09 +0900

    ArgumentResolver 활용 (@Login 어노테이션 생성, 사용)

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -4,16 +4,22 @@ import hello.login.web.Filter.LogFilter;
 import hello.login.web.Filter.LoginCheckFilter;
 import hello.login.web.Intercepter.LogIntercepter;
 import hello.login.web.Intercepter.LoginCheckInterceptor;
+import hello.login.web.argumentResolver.LoginMemberArgumentResolver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -25,7 +31,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(2)
                 .addPathPatterns("/**")
-                .excludePathPatterns(",", "/member/add", "/login", "logout", "/css/**", "/*.ico", "/error");
+                .excludePathPatterns("/", "/member/add", "/login", "logout", "/css/**", "/*.ico", "/error");
 
 
     }
